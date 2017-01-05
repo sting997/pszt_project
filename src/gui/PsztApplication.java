c7232df48deb095825109f910cf58644243cf5aa
Mike <sting997@gmail.com>
2017-01-05 12:27:54 +0100

    added start button

--- src/gui/PsztApplication.java
+++ src/gui/PsztApplication.java
@@ -2,7 +2,6 @@ package gui;
  
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,18 +9,21 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
  
 public class PsztApplication extends Application {	
+	private TextField processorsTextField = new TextField();
+	private TextField populationTextField = new TextField();
+	private TextField mutationTextField = new TextField();
+	private TextField surviveTextField = new TextField();
+	
 	
 	private void init(Stage stage) {
 		Text scenetitle = new Text("Scheduling processes algorithm");
@@ -41,30 +43,41 @@ public class PsztApplication extends Application {
     	Label processorsLabel = new Label("Processors:");
     	grid.add(processorsLabel, 0, 1);
 
-    	TextField processorsTextField = new TextField();
+    	
     	processorsTextField.setPromptText("Enter processors number");
     	grid.add(processorsTextField, 1, 1);
 
-    	Label populationLabel = new Label("Population size:");
+    	Label populationLabel = new Label("Population:");
     	grid.add(populationLabel, 0, 2);
 
-    	TextField populationTextField = new TextField();
+    	
     	populationTextField.setPromptText("Enter population size");
     	grid.add(populationTextField, 1, 2);
     	
     	Label mutationLabel = new Label("Mutation:");
     	grid.add(mutationLabel, 0, 3);
 
-    	TextField mutationTextField = new TextField();
+    	
     	mutationTextField.setPromptText("Enter mutation rate");
     	grid.add(mutationTextField, 1, 3);
     	
     	Label surviveLabel = new Label("Survive:");
     	grid.add(surviveLabel, 0, 4);
 
-    	TextField surviveTextField = new TextField();
+    	
     	surviveTextField.setPromptText("Enter survive rate");
     	grid.add(surviveTextField, 1, 4);
+    	
+    	Button button = new Button("Start");
+        button.setOnAction((ActionEvent e) -> {
+         	//TODO
+        });
+        
+        HBox buttonHbox = new HBox();
+        buttonHbox.setAlignment(Pos.BASELINE_CENTER);
+        buttonHbox.getChildren().add(button);
+        
+        grid.add(buttonHbox, 0, 5, 2, 1);
     	
     	final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
@@ -72,6 +85,9 @@ public class PsztApplication extends Application {
         yAxis.setLabel("Fitness");
         LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
 
+        
+       
+        
         
     	BorderPane border = new BorderPane();
     	border.setTop(hboxTop);
